5dc4bf76d47f4d6a7568e1b39ba82571e6d9826a
snehareddy-kothavenkat <54667663+snehareddy-kothavenkat@users.noreply.github.com>
2020-06-11 16:00:26 +0530

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,44 +1,71 @@
-import keras
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+import numpy as np 
+import keras 
+from keras.datasets import mnist 
+from keras.models import Model 
 from keras.models import Sequential
-from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Input
+from keras.layers import Conv2D, MaxPooling2D, Dropout
+from keras.layers import Flatten 
+from keras import backend as k
+#loading the data
+(x_train, y_train), (x_test, y_test) = mnist.load_data("mymnist.db")
+#shape
+img_rows, img_cols=28, 28
+#setting the input shape
+if k.image_data_format() == 'channels_first': 
+  x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols) 
+  x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols) 
+  inpx = (1, img_rows, img_cols) 
+else: 
+  x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1) 
+  x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1) 
+  inpx = (img_rows, img_cols, 1) 
+#scaling the data
+x_train = x_train.astype('float32') 
+x_test = x_test.astype('float32') 
+x_train /= 255
+x_test /= 255
+#one-hot-encoding
+y_train = keras.utils.to_categorical(y_train) 
+y_test = keras.utils.to_categorical(y_test)
+#creating a model
 model = Sequential()
-#convolution layer 1
-model.add(Convolution2D(filters=32, kernel_size=(3,3), activation='relu',input_shape=(64, 64, 3)))
-#pooling 
-model.add(MaxPooling2D(pool_size=(2, 2))) 
-#Flattening layer
+#convolutional layer1
+model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=inpx))
+#pooling layer 
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#flattening layer
 model.add(Flatten())
-#hidden layer
-model.add(Dense(units=128, activation='relu'))
-#output
-model.add(Dense(units=6, activation='softmax'))
+#hidden layer 
+model.add(Dense(8, activation='relu'))
+#output layer
+model.add(Dense(10, activation='softmax'))
+#summarizing
 model.summary()
-opt = keras.optimizers.Adam(learning_rate=0.0001)
-model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-train_datagen = ImageDataGenerator( rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
-test_datagen = ImageDataGenerator(rescale=1./255)
-training_set = train_datagen.flow_from_directory( '/root/dataset/seg_train', target_size=(64, 64), batch_size=32, class_mode='categorical')
-test_set = test_datagen.flow_from_directory('/root/dataset/seg_test',target_size=(64, 64),batch_size=32,class_mode='categorical')
+#compiling
+model.compile(optimizer=keras.optimizers.Adadelta(), 
+			loss=keras.losses.categorical_crossentropy, 
+			metrics=['accuracy'])
+#callbacks
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-checkpoint = ModelCheckpoint("intel.h5", monitor="val_loss", mode="min", verbose=1)
+checkpoint = ModelCheckpoint("mnist.h5", monitor="val_loss", mode="min", verbose=1)
 earlystop = EarlyStopping(monitor = 'val_loss', min_delta = 0, patience = 10, verbose = 1,restore_best_weights = True)
 callbacks = [earlystop, checkpoint]
-nb_train_samples = 14034
-nb_validation_samples = 3000
+#training model
 epochs = 1
-batch_size = 8
-history = model.fit(training_set,steps_per_epoch = nb_train_samples // batch_size, epochs = epochs,callbacks = callbacks,validation_data=test_set, validation_steps = nb_validation_samples // batch_size)	
-acc = history.history['accuracy']
-l = len(acc)
-final_acc = acc[l-1]
-print(final_acc)
-final_acc1 = 100 * final_acc
-print(final_acc1)
-str_final_acc = str(final_acc1)
-f = open("/accuracy.txt", "w")
+model_history = model.fit(x_train, y_train, epochs = epochs, batch_size=8, verbose = 1)
+#getting model accuracy
+text = model_history.history
+accuracy = text['accuracy'][-1] * 100
+final_acc = int(accuracy)
+#print(final_acc)
+str_final_acc=str(final_acc)
+#print(str_final_acc)
+#saving model accuracy
+f = open("/task3/accuracy.txt", "w")
 f.write(str_final_acc)
 f.close()
+print("Accuracy of the model is :" , final_acc ,"%")
+
+
+
