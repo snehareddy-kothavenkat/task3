59e7faa5cee2f1ea861d526b91c201fedd0dfef5
snehareddy-kothavenkat <54667663+snehareddy-kothavenkat@users.noreply.github.com>
2020-06-11 11:14:25 +0530

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -1,72 +1,44 @@
-import os
-import tensorflow as tf
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import numpy as np 
-import keras 
-from keras.datasets import mnist 
-from keras.models import Model 
+import keras
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
 from keras.models import Sequential
-from keras.layers import Dense, Input
-from keras.layers import Conv2D, MaxPooling2D, Dropout
-from keras.layers import Flatten 
-from keras import backend as k
-#loading the data
-(x_train, y_train), (x_test, y_test) = mnist.load_data(path='/task3/dataset/mnist.npz')
-#shape
-img_rows, img_cols=28, 28
-#setting the input shape
-if k.image_data_format() == 'channels_first': 
-  x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols) 
-  x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols) 
-  inpx = (1, img_rows, img_cols) 
-else: 
-  x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1) 
-  x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1) 
-  inpx = (img_rows, img_cols, 1) 
-#scaling the data
-x_train = x_train.astype('float32') 
-x_test = x_test.astype('float32') 
-x_train /= 255
-x_test /= 255
-#one-hot-encoding
-y_train = keras.utils.to_categorical(y_train) 
-y_test = keras.utils.to_categorical(y_test)
-#creating a model
+from keras.preprocessing.image import ImageDataGenerator
 model = Sequential()
-#convolutional layer1
-model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', input_shape=inpx))
-#pooling layer 
-model.add(MaxPooling2D(pool_size=(2, 2)))
-#flattening layer
+#convolution layer 1
+model.add(Convolution2D(filters=32, kernel_size=(3,3), activation='relu',input_shape=(64, 64, 3)))
+#pooling 
+model.add(MaxPooling2D(pool_size=(2, 2))) 
+#Flattening layer
 model.add(Flatten())
-#hidden layer 
-model.add(Dense(8, activation='relu'))
-#output layer
-model.add(Dense(10, activation='softmax'))
-#summarizing
+#hidden layer
+model.add(Dense(units=128, activation='relu'))
+#output
+model.add(Dense(units=6, activation='softmax'))
 model.summary()
-#compiling
-model.compile(optimizer=keras.optimizers.Adadelta(), 
-			loss=keras.losses.categorical_crossentropy, 
-			metrics=['accuracy'])
-#callbacks
+opt = keras.optimizers.Adam(learning_rate=0.0001)
+model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+train_datagen = ImageDataGenerator( rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory( '/dataset/seg_train', target_size=(64, 64), batch_size=32, class_mode='categorical')
+test_set = test_datagen.flow_from_directory('/dataset/seg_test',target_size=(64, 64),batch_size=32,class_mode='categorical')
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-checkpoint = ModelCheckpoint("model.h5", monitor="val_loss", mode="min", verbose=1)
+checkpoint = ModelCheckpoint("intel.h5", monitor="val_loss", mode="min", verbose=1)
 earlystop = EarlyStopping(monitor = 'val_loss', min_delta = 0, patience = 10, verbose = 1,restore_best_weights = True)
 callbacks = [earlystop, checkpoint]
-#training model
+nb_train_samples = 14034
+nb_validation_samples = 3000
 epochs = 1
-history = model.fit(x_train, y_train, epochs = epochs, batch_size=1, verbose=2)
-#acc = history.history['accuracy']
-#score = model.evaluate(x_test, y_test, verbose=0) 
-#l = len(acc)
-#final_acc = acc[l-1]
-#print(final_acc , flush = True)
-#final_acc1 = 100 * final_acc
-#str_final_acc=str(final_acc1)
-#saving model accuracy
-f = open("/task3/accuracy.txt", "w")
-f.write(100 * history.history['val_accuracy'][-1])
+batch_size = 8
+history = model.fit(training_set,steps_per_epoch = nb_train_samples // batch_size, epochs = epochs,callbacks = callbacks,validation_data=test_set, validation_steps = nb_validation_samples // batch_size)	
+acc = history.history['accuracy']
+l = len(acc)
+final_acc = acc[l-1]
+print(final_acc)
+final_acc1 = 100 * final_acc
+print(final_acc1)
+str_final_acc = str(final_acc1)
+f = open("/accuracy.txt", "w")
+f.write(str_final_acc)
 f.close()
-print("Accuracy of the Trained model is : {} %".format (100 * history.history['val_accuarcy'][-1]))
-model.save('/task3/model.h5')
